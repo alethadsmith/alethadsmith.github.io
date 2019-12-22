4d505b3f234f415563c4edc524129a4f948061fd
alethadsmith <59133892+alethadsmith@users.noreply.github.com>
2019-12-22 00:44:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert("hello")
+document.getElementById("myP").style.fontSize = "xx-large";
+
+$("div").hover(function(){
+  $(this).css("background-color", "yellow");
+  }, function(){
+  $(this).css("background-color", "pink");
+});
