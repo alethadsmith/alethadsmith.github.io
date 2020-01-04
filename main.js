5f3b4236f7f39333ec4ccbb1e824849d981c3abd
Aletha Smith <alethasmith@macbook-pro-226.lan>
2020-01-04 12:39:32 -0800

    message

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-alert("hello")
-document.getElementById("myP").style.fontSize = "xx-large";
+// alert("hello")
+// document.getElementById("myP").style.fontSize = "xx-large";
 
-$("div").hover(function(){
-  $(this).css("background-color", "yellow");
-  }, function(){
-  $(this).css("background-color", "pink");
-});
+// $("div").hover(function(){
+//   $(this).css("background-color", "yellow");
+//   }, function(){
+//   $(this).css("background-color", "pink");
+// });
