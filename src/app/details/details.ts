488d9d28bc90cb6f216ad38d7cafc711276a7181
Ellis Bigelow <ellisbigelow@gmail.com>
2020-01-04 12:32:55 -0800

    Switched to a simple angular app

---
+++ src/app/details/details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'details-page',
+  templateUrl: './details.html',
+  styleUrls: ['./details.css']
+})
+
+export class DetailsComponent {}
